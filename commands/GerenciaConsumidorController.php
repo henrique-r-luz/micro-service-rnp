fa762ddc0f9990be784acb3e6ee44f47a0e59d77
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2023-12-27 10:35:00 -0300

    varios porcessos

--- commands/GerenciaConsumidorController.php
+++ commands/GerenciaConsumidorController.php
@@ -2,19 +2,26 @@
 
 namespace microServiceRnp\commands;
 
+use Yii;
+use yii\helpers\Console;
 use yii\console\Controller;
 
 class GerenciaConsumidorController extends Controller
 {
-    public function __construct()
-    {
-    }
 
+    const urlBase = '/micro-service-rnp';
     public function actionRun(int $totalProcesso)
     {
 
         for ($i = 0; $i < $totalProcesso; $i++) {
-            
+            $this->abriProcessos();
         }
+    }
+    private function abriProcessos()
+    {
+        $consoleCommand = Yii::$app->basePath . '/yii   consumidor/run';
+        $backgroundCommand = Console::isRunningOnWindows() ? 'start /B ' : 'nohup ';
+        $command = $backgroundCommand . ' php ' .  $consoleCommand . ' > ' . '/dev/null 2>&1 &';
+        exec($command);
     }
 }
